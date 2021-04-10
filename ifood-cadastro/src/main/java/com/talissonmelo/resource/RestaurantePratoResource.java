6435077fb0b7e5f44aeb051993de460bd7d70592
TalissonMelo <talissonmelorodrigues@gmail.com>
2021-04-10 09:37:27 -0300

    Criado endPoint para Atualizar e Adicionado Transaction em POST

--- ifood-cadastro/src/main/java/com/talissonmelo/resource/RestaurantePratoResource.java
+++ ifood-cadastro/src/main/java/com/talissonmelo/resource/RestaurantePratoResource.java
@@ -3,8 +3,10 @@ package com.talissonmelo.resource;
 import com.talissonmelo.entity.Prato;
 import com.talissonmelo.entity.Restaurante;
 
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 import java.util.Optional;
 
@@ -21,5 +23,37 @@ public class RestaurantePratoResource {
             throw  new NotFoundException();
         }
         return Prato.list("restaurante", restaurante.get());
+    }
+
+    @POST
+    @Path("{idRestaurante}/pratos")
+    @Transactional
+    public Response adicionarPrato(@PathParam("idRestaurante") Long idRestaurante, Prato dto){
+        Optional<Restaurante> restaurante = Restaurante.findByIdOptional(idRestaurante);
+        if(restaurante.isEmpty()){
+            throw  new NotFoundException();
+        }
+        dto.restaurante = restaurante.get();
+        dto.persist();
+        return Response.status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Path("{idRestaurante}/pratos/{idPrato}")
+    @Transactional
+    public Response editarPrato(@PathParam("idRestaurante") Long idRestaurante, @PathParam("idPrato") Long idPrato , Prato dto){
+        Optional<Restaurante> restaurante = Restaurante.findByIdOptional(idRestaurante);
+        if(restaurante.isEmpty()){
+            throw  new NotFoundException();
+        }
+        Optional<Prato> prato = Prato.findByIdOptional(idPrato);
+        if(prato.isEmpty()){
+            throw  new NotFoundException();
+        }
+        Prato pratoAtualiza = prato.get();
+        pratoAtualiza.price = dto.price;
+        pratoAtualiza.nome = dto.nome;
+        pratoAtualiza.persist();
+        return Response.status(Response.Status.CREATED).build();
     }
 }
